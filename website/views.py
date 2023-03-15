04b01511f00707664c4d6625c0fbcaf4c61f50e3
chidima-asika <chidima27@gmail.com>
2023-03-15 13:22:07 -0400

    Added navbar toggler to html

--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
 # not related to authentification and the user can see, will go here
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #root for home page
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
     
 
