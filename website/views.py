4a8617e87e8c2ce026dd97c43856552ee4543610
chidima-asika <chidima27@gmail.com>
2023-03-17 11:25:45 -0400

    Added in Login Flask Module using LoginManager
    -Can only access home if logged in
    - can only logout if logged in
    - redirects to correct pages

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 # not related to authentification and the user can see, will go here
 
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #root for home page
+@login_required
 def home():
     return render_template("home.html")
     
