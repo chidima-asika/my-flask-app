cf3d1191c137813d54c221d4612d607110fe7dbf
chidima-asika <chidima27@gmail.com>
2023-03-17 11:10:38 -0400

    Allow Users to log in

--- website/auth.py
+++ website/auth.py
@@ -7,6 +7,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password. Please try again', category='error')
+        else:
+            flash('Email does not exist', category='error')
     return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
@@ -21,7 +34,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('User already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First Name must be greater than 1 character.', category='error')
